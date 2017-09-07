98ddda3742bd729a4a2305db4f83dfcc11c2ef06
Marcos Minond <minond.marcos@gmail.com>
2017-09-07 01:05:05 -0600

    fixing import

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"github.com/minond/pixels"
+	"./pixels"
 )
 
 func main() {
 	addr := ":" + os.Getenv("PORT")
-	http.HandleFunc("/pixels", PixelsHandler)
+	http.HandleFunc("/pixels", pixels.PixelsHandler)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
